5eb3fded3d2ad187da1afc4bdf26ada9b321ab56
Yarosvet <000xsz839>
2021-09-01 15:09:14 +0300

    Create db_session.py

---
+++ db/db_session.py
@@ -0,0 +1,27 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
+import sqlalchemy.ext.declarative as dec
+
+SqlAlchemyBase = dec.declarative_base()
+__factory = None
+
+
+def global_init(db_file):
+    global __factory
+
+    if __factory:
+        return
+    if not db_file or not db_file.strip():
+        raise Exception("Database file not found")
+    conn_str = f'sqlite:///{db_file.strip()}?check_same_thread=False'
+    print(f"Connecting to database on {conn_str}")
+    engine = sa.create_engine(conn_str, echo=False)
+    __factory = orm.sessionmaker(bind=engine)
+    import types
+    SqlAlchemyBase.metadata.create_all(engine)
+
+
+def create_session() -> Session:
+    global __factory
+    return __factory()
